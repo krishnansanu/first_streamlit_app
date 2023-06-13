a1516c57496537ee82d9a71611954e321184e035
krishnansanu <krishboss11@gmail.com>
2023-06-13 20:47:19 +1000

    Update streamlit_app.py
    fixing bugs - removing additional lines

--- streamlit_app.py
+++ streamlit_app.py
@@ -69,8 +69,5 @@ if streamlit.button('Add a Fruit to the List'):
     back_from_function = insert_row_snowflake(add_my_fruit)
     streamlit.text(back_from_function)
 
-#This will not work correctly, but just go with it for now
-my_cur.execute("insert into fruit_load_list values('from streamlit')")
-
 #don't run anything past here while we troubleshoot
 #streamlit.stop()
